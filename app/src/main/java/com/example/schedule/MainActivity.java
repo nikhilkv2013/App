1d6c60e64cb121bcb49a8c4d4db06731dbb84753
Nikhil <nikhilk@iastate.edu>
2023-04-15 13:50:02 -0500

    Testing

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -18,7 +18,7 @@ import com.example.schedule.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class Mainctivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
